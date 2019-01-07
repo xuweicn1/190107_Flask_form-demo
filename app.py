faea6b50362677b5d67d6dcb489d7f8fbe836835
xuweicn1 <821382128@qq.com>
2019-01-07 16:55:53 +0800

    v0.1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField,PasswordField
 
 # App config.
 DEBUG = True
@@ -7,24 +7,33 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'SECRET'
 
+class LoginForm(Form):
+    # username = StringField('Name:', validators=[validators.required()])
+    # password = PasswordField('Password:', validators=[validators.required()])
+    s = StringField('设置：')
+    v = StringField('值：')
+
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     form = ReusableForm(request.form)
-    print (form.errors)
-    if request.method == 'POST':
+    if request.method == 'POST' and form.validate():
         name=request.form['name']
         print(name)
+        flash('修改成功', 'success')
+    return render_template('hello1.html', form=form)
 
-    if form.validate():
-    # Save the comment here.
-        flash('Hello ' + name)
-    else:
-        flash('All the form fields are required. ')
-
-    return render_template('hello.html', form=form)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name=request.form['username']
+        password = request.form['password']
+        print(name)
+        print(password)
+    return render_template('login.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
