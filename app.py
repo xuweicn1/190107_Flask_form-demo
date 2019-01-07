3f1bea3f110b8690e8dc4ac18bc3db9d824e7cb0
xuweicn1 <821382128@qq.com>
2019-01-07 13:08:28 +0800

    v0.0

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'SECRET'
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+    print (form.errors)
+    if request.method == 'POST':
+        name=request.form['name']
+        print(name)
+
+    if form.validate():
+    # Save the comment here.
+        flash('Hello ' + name)
+    else:
+        flash('All the form fields are required. ')
+
+    return render_template('hello.html', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
